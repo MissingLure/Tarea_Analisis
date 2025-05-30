973c07e3919353172b30a9c5152e45b0ba06077f
Fabs <fabriziojramos20@gmail.com>
2025-05-29 21:43:31 -0600

    Tarea

---
+++ src/sorts/Sorts.java
@@ -0,0 +1,313 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sorts;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ *
+ * @author fabri
+ */
+public class Sorts {
+
+    /**
+     * @param args the command line arguments
+     */
+    void bubbleSort(int arr[], int n) {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were
+            // swapped by inner loop, then break
+            if (swapped == false) {
+                break;
+            }
+        }
+    }
+
+    private void heapify(int arr[], int n, int i) {
+
+        // Initialize largest as root
+        int largest = i;
+
+        // left index = 2*i + 1
+        int l = 2 * i + 1;
+
+        // right index = 2*i + 2
+        int r = 2 * i + 2;
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        // If largest is not root
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    // Main function to do heap sort
+    void heapSort(int arr[]) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            System.out.println(i);
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    // Function to print an array
+    void printArray(int arr[]) {
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(" \n");
+    }
+
+    private int partition(int[] arr, int low, int high) {
+
+        // Choose the pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and indicates 
+        // the right position of pivot found so far
+        int i = low - 1;
+
+        // Traverse arr[low..high] and move all smaller
+        // elements to the left side. Elements from low to 
+        // i are smaller after every iteration
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        // Move pivot after smaller elements and
+        // return its position
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // Swap function
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // The QuickSort function implementation
+    void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            // pi is the partition return index of pivot
+            int pi = partition(arr, low, high);
+
+            // Recursion calls for smaller elements
+            // and greater or equals elements
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+
+            // Assume the current position holds
+            // the minimum element
+            int min_idx = i;
+
+            // Iterate through the unsorted portion
+            // to find the actual minimum
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+
+                    // Update min_idx if a smaller element
+                    // is found
+                    min_idx = j;
+                }
+            }
+
+            // Move minimum element to its
+            // correct position
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+
+    void InsertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            System.out.println(i);
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void merge(int arr[], int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        // Merge the temp arrays
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sortMerge(int arr[], int l, int r) {
+        if (l < r) {
+
+            // Find the middle point
+            int m = l + (r - l) / 2;
+
+            // Sort first and second halves
+            sortMerge(arr, l, m);
+            sortMerge(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    int[] RandomArray(int n){
+         Random r= new Random();
+         
+         int[] Arr = new int[n];
+         for(int i=0; i<=n-1; i++){
+         Arr[i] = r.nextInt(50000);
+         }
+        
+        return Arr;
+    
+    }
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long startTime = System.nanoTime();
+          
+         int small = 500;
+         int medium = 100000;
+         int big = 1000000;
+                
+           Sorts xd = new Sorts();
+        int arr[] = xd.RandomArray( big );
+
+        int arrSelection[] = arr.clone();
+        int arrInsert[] = arr.clone();
+        int arrHeap[] = arr.clone();
+        int arrMerge[] = arr.clone();
+        int arrBubble[] = arr.clone();
+        int arrQuick[] = arr.clone();
+        
+     
+        
+    //    xd.InsertionSort(arrInsert);
+       xd.bubbleSort(arrBubble, arrBubble.length);
+     //   xd.heapSort(arrHeap);
+      //   xd.quickSort(arrQuick, 0, arrQuick.length - 1);
+       // xd.selectionSort(arrSelection);
+        //xd.sortMerge(arrMerge, 0, arr.length - 1);
+        
+        System.out.println("El array original es: ");
+        xd.printArray(arr);
+        System.out.println("Sort: ");
+        xd.printArray(arrBubble);
+        
+          long endTime = System.nanoTime();
+          double time = (endTime - startTime)/1e6;
+          System.out.println("El tiempo de ejecucion fue: " + time + " ms");
+          System.out.println("El tiempo en segundos : "+ (time/1000) + " segundos");
+        
+    }
+
+}
